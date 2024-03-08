5ea937c92f5c70736458a7d45b104d5474c7b4ca
Hiro <laciferin@gmail.com>
2024-03-08 06:21:41 +0530

    Fix: format and better support

--- getSpec.bash
+++ getSpec.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # RAM
-echo "RAM:"
+echo -n "RAM:"
 free -h | awk '/^Mem:/{print "Total:", $2, "Used:", $3}'
 
 # CPU
-echo "CPU:"
+echo -n  "CPU:"
 echo "Number of CPU cores:" $(nproc)
 echo "CPU model:" $(grep "model name" /proc/cpuinfo | uniq | awk -F: '{print $2}')
 
 # Disk
-echo "Disk:"
-df -h --total | grep "total" | awk '{print "Total:", $2, "Used:", $3}'
+echo -n "Disk: Total: "
+df -h | awk '/^\/dev/{total+=$2; used+=$3} END {printf "%s Used: %s\n", total, used}'
 
 # GPU
-echo "GPU:"
+echo -n "GPU:"
 if command -v nvidia-smi &> /dev/null; then
     nvidia-smi --query-gpu=name --format=csv,noheader
 else
